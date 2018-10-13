947fdbd295109fb8ea8652a25cdf5ba41b9d7a22
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2018-10-13 11:21:17 -0300

    corrigindo caminho do shellscript

--- second/secondPipeline.groovy
+++ second/secondPipeline.groovy
@@ -4,11 +4,11 @@ pipeline{
         stage('Deploy'){
             steps{
                 retry(3){
-                    sh './flakey-deploy.sh'
+                    sh './second/flakey-deploy.sh'
                 }
 
                 timeout(time: 3, unit: 'MINUTES'){
-                    sh './health-check.sh'
+                    sh './second/health-check.sh'
                 }
             }
         }
