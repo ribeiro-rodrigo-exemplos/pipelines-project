08205694b183c664393dcc4e834e02e4bc35d529
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2018-10-13 11:24:09 -0300

    corrigindo permissao shellscript

--- second/secondPipeline.groovy
+++ second/secondPipeline.groovy
@@ -4,10 +4,12 @@ pipeline{
         stage('Deploy'){
             steps{
                 retry(3){
+                    sh 'chmod +x second/flakey-deploy.sh'
                     sh './second/flakey-deploy.sh'
                 }
 
                 timeout(time: 3, unit: 'MINUTES'){
+                    sh 'chmod +x second/health-check.sh'
                     sh './second/health-check.sh'
                 }
             }
