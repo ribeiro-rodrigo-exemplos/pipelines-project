99f2c4f5956eaa50960daae9b620f5e49230b2ce
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2018-10-13 12:09:05 -0300

    quinto pipeline

---
+++ quinto/quintoPipeline.groovy
@@ -0,0 +1,14 @@
+pipeline{
+    agent any
+    environment{
+        DISABLE_AUTH = 'true'
+        DB_ENGINE = 'sqlite'
+    }
+    stages{
+        stage('Build'){
+            steps{
+                sh 'printenv'
+            }
+        }
+    }
+}
