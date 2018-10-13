11ba2dc7b51785b8b72c7133ea96ec482c207379
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2018-10-13 11:34:43 -0300

    corrigindo erro de sintaxe

--- second/terceiroPipeline.groovy
+++ second/terceiroPipeline.groovy
@@ -3,7 +3,7 @@ pipeline{
     stages{
         stage('Deploy'){
             steps{
-                timeout(time:3 unity:'MINUTES'){
+                timeout(time:3, unity:'MINUTES'){
                     retry(5){
                         sh 'chmod +x second/flakey-deploy.sh'
                         sh './second/flakey-deploy.sh'
