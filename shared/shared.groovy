22acc6e51408b1f43aa1f6343f9e4c5ab87059f4
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2018-10-28 11:09:49 -0300

    testando variaveis globais

--- shared/shared.groovy
+++ shared/shared.groovy
@@ -1,4 +1,4 @@
-@Library('my-shared')
+@Library('my-shared') _ 
 import static org.foo.Utilities.*  
 import org.foo.Zot 
 
@@ -8,4 +8,6 @@ def z = new Zot()
 node{
     mvn this, 'clean package'
     z.checkOutFrom 'copiloto-repo'
+    log.info 'Starting'
+    log.warning 'Nothing to do!'
 }
