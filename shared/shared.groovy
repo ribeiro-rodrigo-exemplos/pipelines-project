387b681648df215ef83c203f6e62d6be4c44e7fd
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2018-10-28 10:21:10 -0300

    testando de maneira funcional

--- shared/shared.groovy
+++ shared/shared.groovy
@@ -1,8 +1,12 @@
 @Library('my-shared')
 import org.foo.Utilities 
+@Library('my-shared')
+import org.foo.Zot 
 
 def utils = new Utilities(this); 
+def z = new Zot() 
 
 node{
     utils.mvn 'clean package'
+    z.checkOutFrom 'copiloto-repo'
 }
