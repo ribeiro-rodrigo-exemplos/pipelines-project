c8ee152e2cbbc34aaf6d76b54ce067973ac03cfe
Rodrigo Ribeiro <rodrigo.santos@m2msolutions.com.br>
2018-11-08 13:57:56 -0200

    testando promocao

---
+++ artifactory/Promotion.groovy
@@ -0,0 +1,29 @@
+pipeline{
+    agent any 
+    stages{
+        stage('Test'){
+            steps{
+                script{
+                    
+                    def server = Artifactory.newServer 'http://142.93.254.193:8081/artifactory', 'admin', 'password'
+                    
+                    def promotionConfig = [
+
+                        'buildName' : 'artifactory-pipeline'
+                        'buildNumber' : 6, 
+                        'targetRepo' : 'libs-release-local', 
+                        'comment' : 'aprovando para produção', 
+                        'sourceRepo' : 'libs-snapshot-local', 
+                        'status' : 'Released'
+                        'includeDependencies' : true, 
+                        'copy' : true, 
+                        'failFast' : true 
+
+                    ]
+
+                    server.promote promotionConfig 
+                }
+            }
+        }
+    }
+}
